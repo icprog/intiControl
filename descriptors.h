b8fc1a39aed42d82042367736a44d8a0e74de762
Juan Aguero <juan.aguero@acousticresearch.com.au>
2016-02-12 23:57:11 +1100

    format update

--- descriptors.h
+++ descriptors.h
@@ -105,7 +105,7 @@
         #define GENERIC_IN_EPADDR         (ENDPOINT_DIR_IN  | 1)
 
         /** Endpoint address of the Generic HID reporting OUT endpoint. */
-        #define GENERIC_OUT_EPADDR        (ENDPOINT_DIR_OUT | 2)
+#define GENERIC_OUT_EPADDR        (ENDPOINT_DIR_OUT | 2)
 
         /** Size in bytes of the Generic HID reporting endpoint. */
         #define GENERIC_EPSIZE            24
